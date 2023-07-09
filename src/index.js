9ec21a0ff80ce526fa224c223bb83bccbeef839b
Gabriel Ferreira <willamigabrielf.souza@gmail.com>
2023-07-09 15:19:12 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors({
+    origin: 'https://shiny-cupcake-cc0748.netlify.app/'
+}))
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Home!");
+})
+
+app.post('/', (req, res) => {
+    res.send(req.body)
+});
+
+
+const PORT = 3000;
+app.listen(PORT, ()=> console.log("Server listening on port", PORT));
