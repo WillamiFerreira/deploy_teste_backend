50fe19b6940f999290a16a33c86b31446205b632
Gabriel Ferreira <willamigabrielf.souza@gmail.com>
2023-07-09 17:08:10 -0300

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+app.use(express.json());
+
 app.use(cors({
     origin: 'https://shiny-cupcake-cc0748.netlify.app'
 }))
